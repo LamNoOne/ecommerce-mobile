f24d3c7652ab23d45c4eaa5a134e4d42402b8bec
LamNoOne <trannguyendaclam@gmail.com>
2024-05-03 20:58:57 +0700

    constants

---
+++ app/src/main/java/com/example/ecommercemobile/utils/Constants_Example.kt
@@ -0,0 +1,9 @@
+package com.example.ecommercemobile.utils
+
+object Constants_Example {
+    const val BASE_URL = ""
+    const val PAYMENT_URL = ""
+    const val TOKEN_UID = 1
+    const val PAGE_DEFAULT = 1
+    const val LIMIT_DEFAULT = 10
+}
